77f849fab6a51c6d495d9145c77cc9840599d27b
Dottyyyyy <Orekihoutarou126@gmail.com>
2023-10-17 08:30:20 +0800

    Search Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from './Components/Layout/Header'
-import Footer from './Components/Layout/Footer'
+import Header from './Components/Layouts/Header'
+import Footer from './Components/Layouts/Footer'
 import Home from './Components/Home'
+import ProductDetails from './Components/Product/ProductDetails.js'
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} exact="true" />
+          <Route path="/product/:id" element={<ProductDetails />} exact="true" />
+          <Route path="/search/:keyword" element={<Home />} exact="true" />
         </Routes>
         <Footer />
       </Router>
