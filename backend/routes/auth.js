e910f4b97fd1ea2811cbf18cd6d7f7ee41651067
Dottyyyyy <Orekihoutarou126@gmail.com>
2023-10-19 08:49:05 +0800

    Multer Route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const upload = require("../utils/multer");
+
 const { isAuthenticatedUser,
     authorizeRoles } = require('../middlewares/auth');
 const { registerUser,
@@ -10,16 +12,19 @@ const { registerUser,
     getUserProfile,
     updatePassword,
     updateProfile,
-    allUsers } = require('../controllers/authController');
+    allUsers,
+    getUserDetails } = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/logout', logout);
+
 router.post('/password/forgot', forgotPassword);
 router.put('/password/reset/:token', resetPassword);
 router.get('/me', isAuthenticatedUser, getUserProfile)
 router.put('/password/update', isAuthenticatedUser, updatePassword)
 router.put('/me/update', isAuthenticatedUser, updateProfile)
 router.get('/admin/users', isAuthenticatedUser, allUsers)
+router.get('/admin/user/:id',isAuthenticatedUser, getUserDetails )
 
 module.exports = router;
